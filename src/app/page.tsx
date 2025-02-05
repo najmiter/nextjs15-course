2bcf6d0cfb86869946eadc372eefe34c01c9ae01
Najam <realnajmiter@gmail.com>
2025-02-06 03:04:47 +0500

    comment out the db calls

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,12 @@ import { db } from '@/utils/db';
 import Link from 'next/link';
 
 export default async function Home() {
-  const products = await db.product.createManyAndReturn({
-    data: [
-      { name: 'Product 1', price: 100 },
-      { name: 'Product 2', price: 200 },
-    ],
-  });
+  // const products = await db.product.createManyAndReturn({
+  //   data: [
+  //     { name: 'Product 1', price: 100 },
+  //     { name: 'Product 2', price: 200 },
+  //   ],
+  // });
 
   const res = await fetch('http://localhost:3000/api/get-user', {
     method: 'POST',
@@ -24,7 +24,7 @@ export default async function Home() {
       <h1>Home page</h1>
       <div className="grid gap-2">
         {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
-        <pre>{JSON.stringify(products, null, 2)}</pre>
+        {/* <pre>{JSON.stringify(products, null, 2)}</pre> */}
       </div>
     </main>
   );
