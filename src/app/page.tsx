c1421385de3c48fab83e8e7eeee8d81b7f84a930
Najam <realnajmiter@gmail.com>
2025-02-06 03:04:19 +0500

    db setup with prisma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,14 @@
+import { db } from '@/utils/db';
 import Link from 'next/link';
 
 export default async function Home() {
+  const products = await db.product.createManyAndReturn({
+    data: [
+      { name: 'Product 1', price: 100 },
+      { name: 'Product 2', price: 200 },
+    ],
+  });
+
   const res = await fetch('http://localhost:3000/api/get-user', {
     method: 'POST',
     body: JSON.stringify({
@@ -12,10 +20,11 @@ export default async function Home() {
   const data = await res.json();
 
   return (
-    <main className="text-7xl">
+    <main className="text-2xl">
       <h1>Home page</h1>
       <div className="grid gap-2">
-        <pre>{JSON.stringify(data, null, 2)}</pre>
+        {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
+        <pre>{JSON.stringify(products, null, 2)}</pre>
       </div>
     </main>
   );
